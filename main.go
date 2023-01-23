f51e6b8fa38a9584603f536ba5a5e803000f6d72
Surendra Kandel <surendrakandel@Surendras-Laptop.local>
2023-01-23 13:31:07 -0800

    udpates

--- main.go
+++ main.go
@@ -60,7 +60,7 @@ func main() {
 
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		e.Router.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-			AllowOrigins: []string{"https://firstshipper.com", "https://menuloom.com", "http://localhost:3000", "http://localhost:3001", "http://127.0.0.1:3000", "http://127.0.0.1:8787"},
+			AllowOrigins: []string{"https://firstshipper.com", "https://menuloom.com", "http://localhost:3000", "http://localhost:3001", "http://127.0.0.1:3000", "http://127.0.0.1:8787", "https://api.firstshipper.com"},
 			AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization, "auth-guard"},
 		}))
 		e.Router.OPTIONS("/*", func(c echo.Context) error {
