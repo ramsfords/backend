a79653c6fe03a1756cdac85e269415dea266ae88
Surendra Kandel <surendrakandel@Surendras-Laptop.local>
2023-02-16 22:01:44 -0800

    update

--- foundations/mid/cors.go
+++ foundations/mid/cors.go
@@ -7,33 +7,6 @@ import (
 	"github.com/labstack/echo/v5/middleware"
 )
 
-// func AddHeaders(ctx echo.Context, url string) echo.Context {
-// 	ctx.Request().Header.Set("Content-Type", "*")
-// 	ctx.Request().Header.Set("Access-Control-Allow-Origin", "*")
-// 	ctx.Request().Header.Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Max, Access-Control-Allow-Origin,Access-Control-Allow-Methods,custom_header, pragma")
-// 	ctx.Request().Header.Set("Access-Control-Allow-Credentials", "true")
-// 	ctx.Request().Header.Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
-// 	return ctx
-// }
-// echos.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-// 	AllowOrigins: []string{"http://localhost:3000", "http://127.0.0.1:3000", "https://localhost:3000", "https://firstshipper.com", "https://www.firstshipper.com", "https://menuloom.com", "https://localhost:3001", "https://127.0.0.1:3000", "https://127.0.0.1:8787", "https://api.firstshipper.com"},
-// 	AllowHeaders: []string{
-// 		echo.HeaderOrigin,
-// 		echo.HeaderContentType,
-// 		echo.HeaderAccept,
-// 		echo.HeaderAuthorization,
-// 		"first-access-token",
-// 		"first-refresh-token",
-// 		echo.HeaderAccessControlAllowHeaders,
-// 		echo.HeaderAccessControlRequestHeaders,
-// 		echo.HeaderAccessControlAllowOrigin,
-// 		echo.HeaderAccessControlAllowCredentials,
-// 	},
-// 	AllowMethods: []string{
-// 		http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete, http.MethodOptions, http.MethodHead, http.MethodPatch},
-// 	AllowCredentials: true,
-// }))
-
 func CORS() echo.MiddlewareFunc {
 	return middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"http://localhost:3000", "http://127.0.0.1:3000", "https://localhost:3000", "https://firstshipper.com", "https://www.firstshipper.com", "https://localhost:3001", "https://127.0.0.1:3000", "https://127.0.0.1:8787", "https://api.firstshipper.com"},
